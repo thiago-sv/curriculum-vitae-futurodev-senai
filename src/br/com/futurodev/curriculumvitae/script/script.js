dae2d7bfc35a913d846d0a86c42a8dae8be2fecc
thiago-sv <floriponario@gmail.com>
2022-11-07 11:12:47 -0300

    Commit de atualização

--- src/br/com/futurodev/curriculumvitae/script/script.js
+++ src/br/com/futurodev/curriculumvitae/script/script.js
@@ -1,6 +1,6 @@
 
 
 function alertaNome() {
-    alert("Atualmente em busca da primeira oportunidade como desenvolvedor web front-end.");
+    alert("Atualmente em busca da primeira oportunidade como desenvolvedor web.");
 
 }
